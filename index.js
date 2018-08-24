f18a34615c44e0f8c6110b58122ceb23625909d2
John Paul Schmidt <mc.wren@gmail.com>
2018-08-24 12:56:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const recipes = {spam:'meat'}
+const recipes = {prop:'1'}
 
 function updateObjectWithKeyAndValue(object, key, value) {
 recipes.prop = "1"
@@ -7,8 +7,6 @@ return recipes
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var prop = 'prop'
-  var prop2 = 'prop2'
-  recipes[prop] = '1'
+  recipes[prop2] = '2'
   return recipes;
 }
