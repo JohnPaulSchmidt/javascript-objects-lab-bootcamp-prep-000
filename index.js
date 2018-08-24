7d7c96d08f0a6baeb7d46667934eb2851b101069
John Paul Schmidt <mc.wren@gmail.com>
2018-08-24 13:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ return recipes
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  var obj = {prop:1}
-  var prop2 = 'prop2'
-  obj[prop2] = '2'
-  return recipes
+  object[key]=value
+  return object
 }
