24ab0cc2c80a30677161a3c73d8f00e5f48aa662
John Paul Schmidt <mc.wren@gmail.com>
2018-08-24 12:22:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var recipes = {spam:'meat'}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+recipes.prop = "1"
+recipes.prop2 = "2"
+return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  recipes.prop = '1';
+  recipes.prop2 = '2';
+  return recipes
+}
