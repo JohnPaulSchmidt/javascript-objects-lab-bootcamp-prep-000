b32463ec7ae46ae392b46debf906c47361a7528c
John Paul Schmidt <mc.wren@gmail.com>
2018-08-24 13:46:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  var newObj = destructivelyDeleteFromObjectByKey(object, 'prop');
+  var newObj = destructivelyDeleteFromObjectByKey(recipes, 'prop');
   return newObj
 }
